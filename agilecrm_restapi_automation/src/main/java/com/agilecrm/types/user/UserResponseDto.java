fedbc053bdc31e92af80853a02281bd4d51b81b0
Usha Admin <usha>
2023-02-04 16:22:33 +0530

    Users Pojo classes are added

---
+++ agilecrm_restapi_automation/src/main/java/com/agilecrm/types/user/UserResponseDto.java
@@ -0,0 +1,41 @@
+package com.agilecrm.types.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserResponseDto {
+    private long id;
+    private String name;
+    private String gender;
+    private String email;
+    private String status;
+    @JsonProperty("id")
+    public long getId(){
+        return this.id;
+    }
+    public void setId(long id){
+        this.id=id;
+    }
+    @JsonProperty("name")
+    public String getName() {
+        return this.name; }
+    public void setName(String name) {
+        this.name = name; }
+
+    @JsonProperty("gender")
+    public String getGender() {
+        return this.gender; }
+    public void setGender(String gender) {
+        this.gender = gender; }
+
+    @JsonProperty("email")
+    public String getEmail() {
+        return this.email; }
+    public void setEmail(String email) {
+        this.email = email; }
+
+    @JsonProperty("status")
+    public String getStatus() {
+        return this.status; }
+    public void setStatus(String status) {
+        this.status = status; }
+}
